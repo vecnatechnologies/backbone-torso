96152b37cd479d5897e4646ba44ed3ff62795754
Olivier Dusabimana <diokeyolivier@gmail.com>
2016-02-18 23:38:37 -0500

    Fixed a minor typo
    Fixed minor typo and updated bundle

--- modules/collectionRegistrationMixin.js
+++ modules/collectionRegistrationMixin.js
@@ -157,7 +157,7 @@
            * @method requesterMixin.requesterDispose
            */
           requesterDispose: function() {
-            parentInstance.removeRequster(ownerKey);
+            parentInstance.removeRequester(ownerKey);
           }
         };
 
@@ -209,7 +209,7 @@
       };
 
       /**
-       * @method cacheMixin.removeRequster
+       * @method cacheMixin.removeRequester
        * Removes a requester from this cache. No longer receives updates
        * @param guid {String} the global unique id of the requester
        */
