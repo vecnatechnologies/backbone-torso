bde98188c56c099d5ffad3d9ddfa67b00b6a12cf
kent.willis <kent.willis@vecna.com>
2018-04-23 15:21:47 -0400

    Update docs for v0.8.12

--- torso-bundle.js
+++ torso-bundle.js
@@ -57,28 +57,6 @@
 }));
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
-    define(['backbone'], factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory(require('backbone'));
-  } else {
-    root.Torso = root.Torso || {};
-    root.Torso.history = factory(root.Backbone);
-  }
-}(this, function(Backbone) {
-  'use strict';
-
-  /**
-   * Backbone's history object.
-   * @module    Torso
-   * @class     history
-   * @constructor
-   * @author kent.willis@vecna.com
-   */
-  return Backbone.history;
-}));
-
-(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
     define(['underscore'], factory);
   } else if (typeof exports === 'object') {
     module.exports = factory(require('underscore'));
@@ -258,6 +236,28 @@
     });
   };
 }));
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['backbone'], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory(require('backbone'));
+  } else {
+    root.Torso = root.Torso || {};
+    root.Torso.history = factory(root.Backbone);
+  }
+}(this, function(Backbone) {
+  'use strict';
+
+  /**
+   * Backbone's history object.
+   * @module    Torso
+   * @class     history
+   * @constructor
+   * @author kent.willis@vecna.com
+   */
+  return Backbone.history;
+}));
+
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     define(['backbone', 'backbone.stickit'], factory);
@@ -599,169 +599,6 @@
   };
 
   return templateRenderer;
-}));
-
-(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define([], factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory();
-  } else {
-    root.Torso = root.Torso || {};
-    root.Torso.Mixins = root.Torso.Mixins || {};
-    root.Torso.Mixins.cell = factory();
-  }
-}(this, function() {
-  'use strict';
-  /**
-   * An non-persistable object that can listen to and emit events like a models.
-   * @module Torso
-   * @namespace Torso.Mixins
-   * @class  cellMixin
-   * @author kent.willis@vecna.com
-   */
-  return {
-    /**
-     * Whether a cell can pass as a model or not.
-     * If true, the cell will not fail is persisted functions are invoked
-     * If false, the cell will throw exceptions if persisted function are invoked
-     * @property {Boolean} isModelCompatible
-     * @default false
-     */
-    isModelCompatible: false,
-
-    save: function() {
-      if (!this.isModelCompatible) {
-        throw 'Cell does not have save';
-      }
-    },
-
-    fetch: function() {
-      if (!this.isModelCompatible) {
-        throw 'Cell does not have fetch';
-      }
-    },
-
-    sync: function() {
-      if (!this.isModelCompatible) {
-        throw 'Cell does not have sync';
-      }
-    },
-
-    url: function() {
-      if (!this.isModelCompatible) {
-        throw 'Cell does not have url';
-      }
-    }
-  };
-}));
-
-(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(['backbone'], factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory(require('backbone'));
-  } else {
-    root.Torso = root.Torso || {};
-    root.Torso.Mixins = root.Torso.Mixins || {};
-    root.Torso.Mixins.loading = factory(root.Backbone);
-  }
-}(this, function(Backbone) {
-
-  var $ = Backbone.$;
-
-  /**
-   * Loading logic.
-   *
-   * @module    Torso
-   * @namespace Torso.Mixins
-   * @class  loadingMixin
-   * @author kent.willis@vecna.com
-   */
-  var loadingMixin = function(base) {
-
-    return {
-      /**
-       * Adds the loading mixin
-       * @method constructor
-       * @param args {Object} the arguments to the base constructor method
-       */
-      constructor: function(args) {
-        base.call(this, args);
-        this.loadedOnceDeferred = new $.Deferred();
-        this.loadedOnce = false;
-        this.loadingCount = 0;
-        // Loading is a convenience flag that is the equivalent of loadingCount > 0
-        this.loading = false;
-      },
-
-      /**
-       * @method hasLoadedOnce
-       * @return {Boolean} true if this model/collection has ever loaded from a fetch call
-       */
-      hasLoadedOnce: function() {
-        return this.loadedOnce;
-      },
-
-      /**
-       * @method isLoading
-       * @return {Boolean} true if this model/collection is currently loading new values from the server
-       */
-      isLoading: function() {
-        return this.loading;
-      },
-
-      /**
-       * @method getLoadedOncePromise
-       * @return {Promise} a promise that will resolve when the model/collection has loaded for the first time
-       */
-      getLoadedOncePromise: function() {
-        return this.loadedOnceDeferred.promise();
-      },
-
-      /**
-       * Wraps the base fetch in a wrapper that manages loaded states
-       * @method fetch
-       * @param options {Object} - the object to hold the options needed by the base fetch method
-       * @return {Promise} The loadWrapper promise
-       */
-      fetch: function(options) {
-        return this.__loadWrapper(base.prototype.fetch, options);
-      },
-
-      /**
-       * Base load function that will trigger a "load-begin" and a "load-complete" as
-       * the fetch happens. Use this method to wrap any method that returns a promise in loading events
-       * @method __loadWrapper
-       * @param fetchMethod {Function} - the method to invoke a fetch
-       * @param options {Object} - the object to hold the options needed by the fetchMethod
-       * @return {Promise} a promise when the fetch method has completed and the events have been triggered
-       */
-      __loadWrapper: function(fetchMethod, options) {
-        var object = this;
-        this.loadingCount++;
-        this.loading = true;
-        this.trigger('load-begin');
-        return $.when(fetchMethod.call(object, options)).always(function() {
-          if (!object.loadedOnce) {
-            object.loadedOnce = true;
-            object.loadedOnceDeferred.resolve();
-          }
-          object.loadingCount--;
-          if (object.loadingCount <= 0) {
-            object.loadingCount = 0; // prevent going negative.
-            object.loading = false;
-          }
-        }).done(function(data, textStatus, jqXHR) {
-          object.trigger('load-complete', {success: true, data: data, textStatus: textStatus, jqXHR: jqXHR});
-        }).fail(function(jqXHR, textStatus, errorThrown) {
-          object.trigger('load-complete', {success: false, jqXHR: jqXHR, textStatus: textStatus, errorThrown: errorThrown});
-        });
-      }
-    };
-  };
-
-  return loadingMixin;
 }));
 
 (function(root, factory) {
@@ -1290,6 +1127,169 @@
   };
 
   return mixin;
+}));
+
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.Torso = root.Torso || {};
+    root.Torso.Mixins = root.Torso.Mixins || {};
+    root.Torso.Mixins.cell = factory();
+  }
+}(this, function() {
+  'use strict';
+  /**
+   * An non-persistable object that can listen to and emit events like a models.
+   * @module Torso
+   * @namespace Torso.Mixins
+   * @class  cellMixin
+   * @author kent.willis@vecna.com
+   */
+  return {
+    /**
+     * Whether a cell can pass as a model or not.
+     * If true, the cell will not fail is persisted functions are invoked
+     * If false, the cell will throw exceptions if persisted function are invoked
+     * @property {Boolean} isModelCompatible
+     * @default false
+     */
+    isModelCompatible: false,
+
+    save: function() {
+      if (!this.isModelCompatible) {
+        throw 'Cell does not have save';
+      }
+    },
+
+    fetch: function() {
+      if (!this.isModelCompatible) {
+        throw 'Cell does not have fetch';
+      }
+    },
+
+    sync: function() {
+      if (!this.isModelCompatible) {
+        throw 'Cell does not have sync';
+      }
+    },
+
+    url: function() {
+      if (!this.isModelCompatible) {
+        throw 'Cell does not have url';
+      }
+    }
+  };
+}));
+
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['backbone'], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory(require('backbone'));
+  } else {
+    root.Torso = root.Torso || {};
+    root.Torso.Mixins = root.Torso.Mixins || {};
+    root.Torso.Mixins.loading = factory(root.Backbone);
+  }
+}(this, function(Backbone) {
+
+  var $ = Backbone.$;
+
+  /**
+   * Loading logic.
+   *
+   * @module    Torso
+   * @namespace Torso.Mixins
+   * @class  loadingMixin
+   * @author kent.willis@vecna.com
+   */
+  var loadingMixin = function(base) {
+
+    return {
+      /**
+       * Adds the loading mixin
+       * @method constructor
+       * @param args {Object} the arguments to the base constructor method
+       */
+      constructor: function(args) {
+        base.call(this, args);
+        this.loadedOnceDeferred = new $.Deferred();
+        this.loadedOnce = false;
+        this.loadingCount = 0;
+        // Loading is a convenience flag that is the equivalent of loadingCount > 0
+        this.loading = false;
+      },
+
+      /**
+       * @method hasLoadedOnce
+       * @return {Boolean} true if this model/collection has ever loaded from a fetch call
+       */
+      hasLoadedOnce: function() {
+        return this.loadedOnce;
+      },
+
+      /**
+       * @method isLoading
+       * @return {Boolean} true if this model/collection is currently loading new values from the server
+       */
+      isLoading: function() {
+        return this.loading;
+      },
+
+      /**
+       * @method getLoadedOncePromise
+       * @return {Promise} a promise that will resolve when the model/collection has loaded for the first time
+       */
+      getLoadedOncePromise: function() {
+        return this.loadedOnceDeferred.promise();
+      },
+
+      /**
+       * Wraps the base fetch in a wrapper that manages loaded states
+       * @method fetch
+       * @param options {Object} - the object to hold the options needed by the base fetch method
+       * @return {Promise} The loadWrapper promise
+       */
+      fetch: function(options) {
+        return this.__loadWrapper(base.prototype.fetch, options);
+      },
+
+      /**
+       * Base load function that will trigger a "load-begin" and a "load-complete" as
+       * the fetch happens. Use this method to wrap any method that returns a promise in loading events
+       * @method __loadWrapper
+       * @param fetchMethod {Function} - the method to invoke a fetch
+       * @param options {Object} - the object to hold the options needed by the fetchMethod
+       * @return {Promise} a promise when the fetch method has completed and the events have been triggered
+       */
+      __loadWrapper: function(fetchMethod, options) {
+        var object = this;
+        this.loadingCount++;
+        this.loading = true;
+        this.trigger('load-begin');
+        return $.when(fetchMethod.call(object, options)).always(function() {
+          if (!object.loadedOnce) {
+            object.loadedOnce = true;
+            object.loadedOnceDeferred.resolve();
+          }
+          object.loadingCount--;
+          if (object.loadingCount <= 0) {
+            object.loadingCount = 0; // prevent going negative.
+            object.loading = false;
+          }
+        }).done(function(data, textStatus, jqXHR) {
+          object.trigger('load-complete', {success: true, data: data, textStatus: textStatus, jqXHR: jqXHR});
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+          object.trigger('load-complete', {success: false, jqXHR: jqXHR, textStatus: textStatus, errorThrown: errorThrown});
+        });
+      }
+    };
+  };
+
+  return loadingMixin;
 }));
 
 (function(root, factory) {
